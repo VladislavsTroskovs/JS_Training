3f9590d3eddf798ca7c7fbd847e26d174b03cbca
VladislavsTroskovs <troskov10@inbox.lv>
2025-11-20 14:14:52 +0200

    Payment calculation

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,9 +1,10 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'; //ESM Javascript (also external library)
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import {renderPaymentSummary} from './paymentSummary.js';
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'day');
@@ -17,19 +18,12 @@ export function renderOrderSummary(){
 
     cart.forEach((cartItem)=>{
         const productId = cartItem.productId;
-        let matchingProduct;
-
-        products.forEach((product)=>{
-            if(product.id === productId) {
-                matchingProduct = product;
-            }
-        });
+        const matchingProduct = getProduct(productId);
         
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
-        deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId) {deliveryOption = option;}
-        });
+
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
+        
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
         const dateString = deliveryDate.format('dddd, MMMM D');
@@ -109,6 +103,7 @@ export function renderOrderSummary(){
             removeFromCart(productId);
 
             document.querySelector(`.js-cart-item-container-${productId}`).remove();    /*remove this block from page (dont need to refresh)*/
+            renderPaymentSummary();
         });
     });
     document.querySelectorAll('.js-delivery-option').forEach((element)=>{
@@ -116,6 +111,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
